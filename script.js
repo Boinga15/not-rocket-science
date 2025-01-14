fed50d095ca2d943613cb966d3dfc35ce8600614
Boinga15 <realboinga@gmail.com>
2025-01-14 12:42:05 +0000

    Added Stages 4 and 5. Also started ARG with Backdoor implementation.

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 let systemStabilityResolution = 20;
 
 // Global variables
-var currentStage = 1;
+var currentStage = 0;
 var systemStability = 5000;
 var pressesToNextStage = 1;
+
+var systemCode = "AGX51"
 
 var stage3Button = 0;
 
@@ -42,6 +44,7 @@ var mainLoop = window.setInterval(function() {
     updateScreen();
 }, 1);
 
+
 // Repair Button
 function repairButton() {
     systemStability += 1 + Math.floor(Math.random() * 799);
@@ -56,6 +59,13 @@ function advanceButton(buttonID) {
     console.log("You tried tbh.")
 
     if (currentStage >= 2 && (buttonID != stage3Button)) {
+
+
+    } else if (currentStage >= 4 && (document.getElementById("systemCodeParagrpah").innerHTML != document.getElementById("systemCodeInput").value)) {
+        // Backdoor Entry
+        if (currentStage >= 4 && (document.getElementById("systemCodeInput").value == "UF52BZ629NBXZ4")) {
+            window.location.href = "backdoor/entry.html";
+        }
 
     } else {
         console.log("Pressed.")
@@ -91,12 +101,14 @@ function handleStage() {
             break;
         
         case 1:
-            pressesToNextStage = 20;
+            //pressesToNextStage = 20;
+            pressesToNextStage = 1;
             helpTextRef.innerHTML = "Good job. You've probably noticed that the integrity has started to rapidly degrade. Make sure to keep it topped up while you read these messages. Go on and keep clicking.";
             break;
 
         case 2:
-            pressesToNextStage = 15;
+            //pressesToNextStage = 10;
+            pressesToNextStage = 1;
             helpTextRef.innerHTML = "Still too easy? Alright, let's spice things up. There are now two buttons that could advance stages. One is coloured green, one isn't. Click the green one, please.";
             
             let newButton = document.createElement("button");
@@ -111,5 +123,45 @@ function handleStage() {
 
             document.getElementById("advanceButton1").style = "color: green;";
             break;
+        
+            case 3:
+                //pressesToNextStage = 10;
+                pressesToNextStage = 1;
+                helpTextRef.innerHTML = "I'm sure you're wondering if this has a point or not. If any of these buttons rea;;y matter. Well it doesn't matter. At least, not to you.";
+                break;
+            
+            case 4:
+                pressesToNextStage = 10;
+                helpTextRef.innerHTML = "Alright, new gimmick? New gimmick. Makes sure you keep an eye on the System Code. If it changes, copy it into the text field next to the code. Copy and paste, type it out, I don't care how you get it in there, just get it in there. That activate button won't work if you've got the wrong code."
+                
+                let newParagraph = document.createElement("p");
+                newParagraph.id = "systemCodeParagrpah"
+                document.getElementById("systemCodeArea").appendChild(newParagraph)
+                
+                let newField = document.createElement("input");
+                newField.type = "text";
+                newField.id = "systemCodeInput";
+                document.getElementById("systemCodeArea").append(newField);
+
+                newParagraph.innerHTML = systemCode;
+                break;
     }
 }
+
+
+// Steag 5 ---> Randomise Code
+var codeLoop = window.setInterval(function() {
+
+    if(Math.random() > 0.55) {
+        var components = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        systemCode = "";
+
+        for(let i = 0; i < 5; i++) {
+            systemCode += components[Math.floor(Math.random() * components.length)];
+        }
+        if (document.getElementById("systemCodeParagrpah") != null) {
+            document.getElementById("systemCodeParagrpah").innerHTML = systemCode;
+        }
+    }
+
+}, 4897);
